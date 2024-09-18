d5d1e77b3579f5a1622de54bbc8881a50d23a127
BuenVia <matthewclifford@hotmail.co.uk>
2024-09-18 21:00:09 +0100

    Renamed project from back_end to learnx

--- api/urls.py
+++ api/urls.py
@@ -7,30 +7,30 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('', views.index, name="index"),
     # User
-    path('api/user/', views.UserViewSet.as_view(), name='user-create'),
+    path('user/', views.UserViewSet.as_view(), name='user-create'),
     # JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Subjects
-    path('api/subject/', views.SubjectViewSet.as_view()),
-    path('api/subject/<int:pk>', views.SubjectViewSet.as_view()),
+    path('subject/', views.SubjectViewSet.as_view()),
+    path('subject/<int:pk>', views.SubjectViewSet.as_view()),
     # Categories
-    path('api/categories/', views.CategoriesViewSet.as_view(), name='category-list'),
-    path('api/categories/<int:pk>/', views.CategoriesViewSet.as_view(), name='category-detail'),
+    path('categories/', views.CategoriesViewSet.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoriesViewSet.as_view(), name='category-detail'),
     # path('api/category/<id>', views.CategoryViewSet.as_view()),
     # Learn
-    path('api/learn/', views.LearnViewSet.as_view(), name='learn-list'),  # To get all Learn objects (optional)
-    path('api/learn/category/<int:category_id>/', views.LearnViewSet.as_view(), name='learn-by-category'),  # To get Learn objects by category
-    path('api/learn/<int:pk>/', views.LearnViewSet.as_view(), name='learn-detail'),  # To get/update/delete a specific Learn object
+    path('learn/', views.LearnViewSet.as_view(), name='learn-list'),  # To get all Learn objects (optional)
+    path('learn/category/<int:category_id>/', views.LearnViewSet.as_view(), name='learn-by-category'),  # To get Learn objects by category
+    path('learn/<int:pk>/', views.LearnViewSet.as_view(), name='learn-detail'),  # To get/update/delete a specific Learn object
     # Practice
 
-    path('api/practice-sections/', views.PracticeSectionsViewSet.as_view(), name='practice-sections-list'),  # To get all PracticeSections or create a new one
-    path('api/practice-sections/category/<int:category_id>/', views.PracticeSectionsViewSet.as_view(), name='practice-sections-by-category'),  # To get PracticeSections by category
-    path('api/practice-sections/<int:pk>/', views.PracticeSectionsViewSet.as_view(), name='practice-section-detail'),  # To get/update/delete a specific PracticeSection
+    path('practice-sections/', views.PracticeSectionsViewSet.as_view(), name='practice-sections-list'),  # To get all PracticeSections or create a new one
+    path('practice-sections/category/<int:category_id>/', views.PracticeSectionsViewSet.as_view(), name='practice-sections-by-category'),  # To get PracticeSections by category
+    path('practice-sections/<int:pk>/', views.PracticeSectionsViewSet.as_view(), name='practice-section-detail'),  # To get/update/delete a specific PracticeSection
 
     # path('api/practice/all/<id>', views.PracticeSectionsViewSet.as_view()),
     # path('api/practice/<id>', views.LearnViewSet.as_view()),
-    path('api/questions/<id>', views.QuestionsViewSet.as_view()),
+    path('questions/<id>', views.QuestionsViewSet.as_view()),
     # Test
-    path('api/test-data/<id>', views.LearnViewSet.as_view()),
+    path('test-data/<id>', views.LearnViewSet.as_view()),
 ]
--- learnx/urls.py
+++ learnx/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for back_end project.
+URL configuration for learnx project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
     # path('api/token/', include('rest_framework_simplejwt.urls')),
 ]
