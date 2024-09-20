ea08bb96a22d3ba96314979b5bb0e95f940beb1a
BuenVia <matthewclifford@hotmail.co.uk>
2024-09-20 07:44:12 +0100

    update

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("register", views.register, name='register'),
+    path("register/", views.register, name='register'),
     path("dashboard", views.dashboard, name='dashboard'),
     path("account/", views.account, name='account'),
     # path("property-create", views.property_create, name="property-create"),
